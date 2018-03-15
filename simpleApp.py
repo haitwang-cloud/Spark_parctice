4af9fb16ded1ddb33563272ef8bef4fd672d43fa
wanghaitao <wanghatiao@i.shu.edu.cn>
2018-03-15 09:51:08 +0800

    add demo and practice

--- simpleApp.py
+++ simpleApp.py
@@ -1,12 +1,15 @@
 from pyspark.sql import SparkSession
 
 logFile="/home/wht/software/spark/README.md"
-spark = SparkSession.builder().appName(appName).master(master).getOrCreate()
-logData=spark.read.text(logFile).cathe()
+spark = SparkSession\
+        .builder\
+        .appName("PythonWordCount")\
+        .getOrCreate()
+logData = spark.read.text(logFile).cache()
 
-numAs=logData.filter(logData.value/contains('a')).count()
-numBs=logData.filter(logData.value/contains('b')).count()
+numAs=logData.filter(logData.value.contains('a')).count()
+numBs=logData.filter(logData.value.contains('b')).count()
 
-print("Lines with a: %i,lines with b: %i"(numAs,numBs))
+print("Lines with a: %i,lines with b: %i" %(numAs,numBs))
 
 spark.stop()
